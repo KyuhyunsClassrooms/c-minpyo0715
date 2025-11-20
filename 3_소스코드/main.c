caefd978e50580c600a63e04ae8da4e8f17bf9f0
minpyo0715 <gwanggyo.s240272@ggh.goe.go.kr>
2025-11-20 05:37:03 +0000

    코딩

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
 void func1() {
-    printf(">> func1 실행!\n");
+    printf(">> 역학시뮬레이션 실행!\n");
+    printf("순서대로 가속도, 초기속도, 운동시간을 입력해주세요(정수)\n");
+    int a, v, t, s;
+    
+    scanf("%d %d %d", &a, &v, &t);
+    
+
+    s = (0.5 * a * t * t) + (v * t);
+    printf("물체의 위치는 %d입니다\n" , s);
+
+  
+
+
 }
 
 void func2() {
-    printf(">> func2 실행!\n");
+    printf(">> 파동 주기 계산기 실행!\n");
+    printf("순서대로 파장과 속도를 입력해주세요");
+    int w, v, s;
+
+    scanf("%f %f %f" &w, %v, &s);
+    
+
+
 }
 
 int main() {
@@ -13,8 +32,8 @@ int main() {
 
     // 함수 선택 메뉴 출력
     printf("=== 함수 선택 ===\n");
-    printf("1. func1 실행\n");
-    printf("2. func2 실행\n");
+    printf("1. 역학시뮬레이션 실행\n");
+    printf("2. 파동 주기 계산기 실행\n");
     printf("번호 입력: ");
     scanf("%d", &select);
 
@@ -27,4 +46,6 @@ int main() {
         printf("잘못된 선택입니다.\n");
 
     return 0;
+
+    
 }
